eb1b485ff4e47c40b7e647adcf4ac797587728ba
2222-42 <stephen0617@me.com>
2020-06-20 23:09:49 +0900

    chore(chap2): finish 2.3.2

--- ConcreteSemanticsChapter2/Concrete_Semantics_2_3.thy
+++ ConcreteSemanticsChapter2/Concrete_Semantics_2_3.thy
@@ -2,6 +2,21 @@ theory Concrete_Semantics_2_3
   imports Main
 begin
 
+(* 2.3 *)
+(* Type synonyms are abbreviations for existing types, *)
+(* Type synonyms are expanded after parsing and are not present in internal
+representation and output. *)
+
+(* 2.3.1 *)
+(* It introduces the constructors C_{i} :: T_{i,1} => ... => T{i,n_{i}} => ( 'a_{1}, ..., 'a_{n})t
+and expresses that any value of this type is built from these constructors in
+a unique manner. *)
+(*
+niqueness is implied by the following properties of the
+constructors:
+- Distinctness
+- Injectivity
+*)
 datatype 'a tree = Tip | Node "'a tree" 'a "'a tree"
 
 fun mirror :: "'a tree => 'a tree" where
@@ -18,5 +33,12 @@ datatype 'a option = None | Some 'a
 fun lookup :: "('a * 'b) list => 'a => 'b option" where
 "lookup [] x = None" |
 "lookup ((a, b) # ps) x = (if a = x then Some b else lookup ps x)"
+
+(* 2.3.2 *)
+(* Non-recursive functions can be defined. *)
+(* Such definitions do not allow pattern matching but only f x_1 ... x_n = t *)
+
+definition sq :: "nat => nat" where
+"sq n = n * n"
 
 end
