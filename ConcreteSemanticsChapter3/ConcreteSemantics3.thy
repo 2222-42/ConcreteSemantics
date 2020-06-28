e6aae314523196ab4d5215abdaab5e27c40be0aa
2222-42 <stephen0617@me.com>
2020-06-28 09:55:50 +0900

    chore(chap3): finish 3.1.2

--- ConcreteSemanticsChapter3/ConcreteSemantics3.thy
+++ ConcreteSemanticsChapter3/ConcreteSemantics3.thy
@@ -31,4 +31,29 @@ They are not same:
 
 subsubsection "3.1.2 Semantics"
 
+(* 
+The value of all variables is recorded in the (program) state 
+The state is a function from variable names to values
+*)
+
+type_synonym val = int
+type_synonym state = "vname => val"
+
+fun aval :: "aexp => state => val" where
+"aval (N n) s = n" |
+"aval (V x) s = s x" |
+"aval (Plus a1 a2) s = aval a1 s + aval a2 s"
+
+value "aval (Plus (N 3) (V ''x'')) (\<lambda>x.0)"
+
+(* 
+the generic function update notation f (a := b) is used: 
+    the result is the same as f, except that it maps a to b:
+        f (a := b) = (\<lambda>x. if x = a then b else f x )
+
+"((\<lambda>x.0) (''x'' := 7))(''y'' := 3)" maps ''x'' to 7, ''y'' to 3
+<''x'' := 7; ''y'' := 3>: which works for any number of variables, even for none: <> is syntactic sugar for \<lambda>x : 0.
+*)
+
+
 end
