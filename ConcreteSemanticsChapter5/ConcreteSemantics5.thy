ad6a232b68c9aa507255d4bf6a103c4be8f0da76
2222-42 <stephen0617@me.com>
2020-08-01 08:49:10 +0900

    feat(chap5): finish Ex5.2

--- ConcreteSemanticsChapter5/ConcreteSemantics5.thy
+++ ConcreteSemanticsChapter5/ConcreteSemantics5.thy
@@ -273,7 +273,7 @@ subsubsection "5.3.3 Local Lemmas"
     <proof>
 *)
 
-
+thm dvd_def
 
 lemma fixes a b :: int assumes "b dvd (a + b)" shows "b dvd a"
 proof - 
@@ -305,5 +305,30 @@ next
   then show "T x y" using \<open>T y x\<close> by auto
 qed
 qed
+
+(* Exercise 5.2 *)
+lemma "\<exists> ys zs. xs = ys @ zs \<and> 
+        ( length ys = length zs \<or>  length ys = length zs + 1)"
+proof cases
+  assume "even (length xs)"
+  then obtain a where "length xs = 2 * a" by auto
+    let ?ys = "take a xs"
+    let ?zs = "drop a xs"
+    have "xs = ?ys @ ?zs \<and>length ?ys = length ?zs" by(simp add: `length xs = 2 * a`)
+    hence "xs = ?ys @ ?zs \<and>
+        (length ?ys = length ?zs \<or> length ?ys = length ?zs + 1)" by (auto)
+    thus ?thesis by blast
+next
+  assume "odd (length xs)"
+  then obtain a where "length xs = (2 * a) + 1" 
+    using oddE by blast
+    let ?ys = "take (a + 1) xs"
+    let ?zs = "drop (a + 1) xs"
+    have "xs = ?ys @ ?zs \<and> length ?ys = length ?zs + 1" by (simp add: \<open>length xs = 2 * a + 1\<close>)
+    hence "xs = ?ys @ ?zs \<and>
+        (length ?ys = length ?zs \<or> length ?ys = length ?zs + 1)" by (auto)
+    thus ?thesis  by blast
+qed
+
 
 end
