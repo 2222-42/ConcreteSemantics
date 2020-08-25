d02708f2f379d3c39f12c22b4d8fdaf82ad6f9f8
2222-42 <stephen0617@me.com>
2020-08-26 07:48:28 +0900

    fix(Ex7.9): modify order of Com

--- ConcreteSemanticsChapter7/ex7/ConcreteSemantics_7_Ex10/Com.thy
+++ ConcreteSemanticsChapter7/ex7/ConcreteSemantics_7_Ex10/Com.thy
@@ -8,7 +8,7 @@ datatype
       | Seq    com  com         ("_;;/ _"  [60, 61] 60)
       | If     bexp com com     ("(IF _/ THEN _/ ELSE _)"  [0, 0, 61] 61)
       | While  bexp com         ("(WHILE _/ DO _)"  [0, 61] 61)
-      | Throw                (*what is type of exception and its order?*)
-      | TryCatch  com  com      ("(TRY _/ CATCH _)"  [60, 61] 60)
+      | Throw                (*what is type of exception and its order? -> nothing. cf: http://www21.in.tum.de/~nipkow/Concrete-Semantics/Exercises/exercises.pdf *)
+      | Try    com  com         ("(TRY _/ CATCH _)"  [0, 61] 60)
 
 end
