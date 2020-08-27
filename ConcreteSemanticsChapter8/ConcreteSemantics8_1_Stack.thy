7f46e4ba9482c4aaee71aae7797a7cc3bb221981
2222-42 <stephen0617@me.com>
2020-08-28 08:00:22 +0900

    chore(chap8): finish Chap8.1

--- ConcreteSemanticsChapter8/ConcreteSemantics8_1_Stack.thy
+++ ConcreteSemanticsChapter8/ConcreteSemantics8_1_Stack.thy
@@ -36,7 +36,57 @@ This problem is solved by set declare in top of this theory.
 \<close>
 lemma "0 \<le> i \<Longrightarrow> (xs @ ys) !! i = (if i < size xs then xs !! i else ys !! (i - size xs))"
   apply(induction xs arbitrary: i)
+  (* Unfortunately, the following method is not derived only by sledgehammer *)
    apply (auto simp add: algebra_simps)
   done
+
+abbreviation (output)
+  "isize xs == int (length xs)"
+
+notation isize ("size")
+
+datatype instr =
+  LOADI int | LOAD vname | ADD | STORE vname |
+  JMP int | JMPLESS int | JMPGE int
+
+type_synonym stack = "val list"
+type_synonym config = "int \<times> state \<times> stack"
+
+abbreviation "hd2 xs \<equiv> hd (tl xs)"
+abbreviation "tl2 xs \<equiv> tl (tl xs)"
+
+fun iexec :: "instr \<Rightarrow> config \<Rightarrow> config" where
+"iexec instr (i,s,stk) = (case instr of
+  LOADI n \<Rightarrow> (i+1, s, n # stk) |
+  LOAD x \<Rightarrow> (i +1, s, s x # stk) |
+  ADD \<Rightarrow> (i+1, s, (hd2 stk + hd stk) # tl2 stk) |
+  STORE x \<Rightarrow> (i + 1, s(x := hd stk), tl stk) |
+  JMP n \<Rightarrow> (i + 1 + n, s, stk)| 
+  JMPLESS n \<Rightarrow> (if hd2 stk < hd stk then i + 1 + n else i + 1, s, tl2 stk)| 
+  JMPGE n \<Rightarrow>  (if hd stk \<le> hd2 stk then i + 1 + n else i + 1, s, tl2 stk)
+)"
+
+definition exec1 :: "instr list \<Rightarrow> config \<Rightarrow> config \<Rightarrow> bool" ("(_/ \<turnstile> (_ \<rightarrow>/ _))" [59,0,59] 60)where
+"P \<turnstile> c \<rightarrow> c' =
+(\<exists>i s stk. c = (i,s,stk) \<and> c' = iexec(P!!i) (i,s,stk) \<and> 0 \<le> i \<and> i < size P)"
+
+lemma exec1I [intro, code_pred_intro]:
+  "c' = iexec (P!!i) (i,s,stk) \<Longrightarrow> 0 \<le> i \<Longrightarrow> i < size P
+  \<Longrightarrow> P \<turnstile> (i,s,stk) \<rightarrow> c'"
+  by (simp add: exec1_def)
+
+abbreviation exec :: "instr list \<Rightarrow> config \<Rightarrow> config \<Rightarrow> bool" ("(_/ \<turnstile> (_ \<rightarrow>*/ _))" [59,0,59] 60)where
+"exec P \<equiv> star (exec1 P)"
+
+lemmas exec_induct = star.induct [of "exec1 P", split_format(complete)]
+
+text\<open>this code_pred are not proved by using `metis` without the above lemma "exec1I"\<close>
+code_pred exec1 by (metis exec1_def)
+
+text\<open>
+Without `code_pred exec1`, Following values are not calculated by the following reason:
+  No mode possible for comprehension
+\<close>
+values "{(i, map t [''x'', ''y''], stk ) | i t stk. [LOAD ''y'', STORE ''x''] \<turnstile> (0, <''x'' := 3, ''y'' := 4>, []) \<rightarrow>* (i, t, stk )}"
 
 end
