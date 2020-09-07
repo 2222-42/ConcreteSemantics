e7df40658105d833d70c638db185c85909370a5f
2222-42 <stephen0617@me.com>
2020-09-07 13:40:45 +0900

    chore(chap9): add tbval and commands

--- ConcreteSemanticsChapter9/ConcreteSemantics9_1_Typed.thy
+++ ConcreteSemanticsChapter9/ConcreteSemantics9_1_Typed.thy
@@ -28,8 +28,43 @@ inductive_cases [elim!]:
   "taval (V x) s v"
   "taval (Plus a1 a2) s v"
 
-value "taval (Plus (Ic 3) (V ''x'')) (\<lambda>x. if x = ''x'' then Iv 4 else I 0) (Iv 7)"
-value "taval (Plus (Ic 3) (V ''x'')) ((\<lambda>x. (I 0)) (''x'' := Iv 4)) (Iv 7)"
-value "taval (Plus (Ic 3) (V ''x'')) ((\<lambda>x. (R 0)) (''x'' := Rv 4)) (Iv 7)"
+subsection "Boolean Expressions"
+
+datatype bexp = Bc bool | Not bexp | And bexp bexp | Less aexp aexp
+
+inductive tbval :: "bexp \<Rightarrow> state \<Rightarrow> bool \<Rightarrow> bool" where
+"tbval (Bc v) s v" |
+"tbval b s bv \<Longrightarrow> tbval (Not b) s (\<not> bv)" |
+"tbval b1 s bv1 \<Longrightarrow> tbval b2 s bv2 \<Longrightarrow> tbval (And b1 b2) s (bv1 & bv2)" |
+"taval a1 s (Iv i1) \<Longrightarrow> taval a2 s (Iv i2) \<Longrightarrow> tbval (Less a1 a2) s (i1 < i2)" |
+"taval a1 s (Rv r1) \<Longrightarrow> taval a2 s (Rv r2) \<Longrightarrow> tbval (Less a1 a2) s (r1 < r2)"
+
+subsection "Syntax of Commands"
+(* a copy of Com.thy - keep in sync! *)
+
+datatype
+  com = SKIP 
+      | Assign vname aexp       ("_ ::= _" [1000, 61] 61)
+      | Seq    com  com         ("_;; _"  [60, 61] 60)
+      | If     bexp com com     ("IF _ THEN _ ELSE _"  [0, 0, 61] 61)
+      | While  bexp com         ("WHILE _ DO _"  [0, 61] 61)
+
+
+subsection "Small-Step Semantics of Commands"
+
+inductive
+  small_step :: "(com \<times> state) \<Rightarrow> (com \<times> state) \<Rightarrow> bool" (infix "\<rightarrow>" 55)
+where
+Assign:  "taval a s v \<Longrightarrow> (x ::= a, s) \<rightarrow> (SKIP, s(x := v))" |
+
+Seq1:   "(SKIP;;c,s) \<rightarrow> (c,s)" |
+Seq2:   "(c1,s) \<rightarrow> (c1',s') \<Longrightarrow> (c1;;c2,s) \<rightarrow> (c1';;c2,s')" |
+
+IfTrue:  "tbval b s True \<Longrightarrow> (IF b THEN c1 ELSE c2,s) \<rightarrow> (c1,s)" |
+IfFalse: "tbval b s False \<Longrightarrow> (IF b THEN c1 ELSE c2,s) \<rightarrow> (c2,s)" |
+
+While:   "(WHILE b DO c,s) \<rightarrow> (IF b THEN c;; WHILE b DO c ELSE SKIP,s)"
+
+lemmas small_step_induct = small_step.induct[split_format(complete)]
 
 end
