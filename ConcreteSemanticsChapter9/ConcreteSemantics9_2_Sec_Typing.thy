4a8cf20fa4e265c154f167c20ff3cb6c1ee33c1e
2222-42 <stephen0617@me.com>
2020-09-23 13:57:30 +0900

    chore(chap9): add sec_type

--- ConcreteSemanticsChapter9/ConcreteSemantics9_2_Sec_Typing.thy
+++ ConcreteSemanticsChapter9/ConcreteSemantics9_2_Sec_Typing.thy
@@ -2,4 +2,27 @@ theory ConcreteSemantics9_2_Sec_Typing
   imports Main "~~/src/HOL/IMP/Sec_Type_Expr" 
 begin 
 
+subsubsection "Syntax Directed Typing"
+
+inductive sec_type :: "nat \<Rightarrow> com \<Rightarrow> bool" ("(_/ \<turnstile> _)" [0,0] 50) where
+Skip:
+  "l \<turnstile> SKIP" |
+Assign:
+  "\<lbrakk> sec x \<ge> sec a;  sec x \<ge> l \<rbrakk> \<Longrightarrow> l \<turnstile> x ::= a" |
+Seq:
+  "\<lbrakk> l \<turnstile> c\<^sub>1;  l \<turnstile> c\<^sub>2 \<rbrakk> \<Longrightarrow> l \<turnstile> c\<^sub>1;;c\<^sub>2" |
+If:
+  "\<lbrakk> max (sec b) l \<turnstile> c\<^sub>1;  max (sec b) l \<turnstile> c\<^sub>2 \<rbrakk> \<Longrightarrow> l \<turnstile> IF b THEN c\<^sub>1 ELSE c\<^sub>2" |
+While:
+  "max (sec b) l \<turnstile> c \<Longrightarrow> l \<turnstile> WHILE b DO c"
+
+code_pred (expected_modes: i => i => bool) sec_type .
+
+value "0 \<turnstile> IF Less (V ''x1'') (V ''x'') THEN ''x1'' ::= N 0 ELSE SKIP"
+value "0 \<turnstile> IF Less (V ''x1'') (V ''x'') THEN ''x''  ::= N 0 ELSE SKIP"
+value "1 \<turnstile> IF Less (V ''x1'') (V ''x'') THEN ''x''  ::= N 0 ELSE SKIP"
+value "1 \<turnstile> IF Less (V ''x1'') (V ''x'') THEN ''x1'' ::= N 0 ELSE SKIP"
+value "2 \<turnstile> IF Less (V ''x1'') (V ''x'') THEN ''x1'' ::= N 0 ELSE SKIP"
+value "3 \<turnstile> IF Less (V ''x1'') (V ''x'') THEN ''x1'' ::= N 0 ELSE SKIP"
+
 end
