cbd0d96850d1de6704d76f1be04d87a171a51003
2222-42 <stephen0617@me.com>
2020-10-05 07:50:37 +0900

    chore(chap9): give up of proving Exercise 9.7

--- ConcreteSemanticsChapter9/ConcreteSemantics9_2_Sec_Typing.thy
+++ ConcreteSemanticsChapter9/ConcreteSemantics9_2_Sec_Typing.thy
@@ -377,5 +377,135 @@ next
 qed (auto intro: sec_type2'.intros)
 
 
+(* Exercise 9.7. *)
+fun erase :: "level \<Rightarrow> com \<Rightarrow> com" where
+  "erase l (SKIP) = SKIP"
+  |"erase l (x ::= a) = (if (sec x \<ge> l) then SKIP else x ::= a)"
+  | "erase l (Seq com1 com2) = Seq (erase l com1) (erase l com2)"
+  | "erase l (If bexp com1 com2) = (if (sec bexp \<ge> l) then SKIP else (If bexp (erase l com1) (erase l com2)))"
+  |"erase l (While  bexp com) = (if sec bexp \<ge> l then SKIP else (While bexp (erase l com)))"
 
+
+(*
+  "\<lbrakk> (c,s) \<Rightarrow> s'; (c,t) \<Rightarrow> t';  0 \<turnstile> c;  s = t (\<le> l) \<rbrakk>
+   \<Longrightarrow> s' = t' (\<le> l)"
+*)
+lemma "\<lbrakk>(c, s) \<Rightarrow> s'; (erase l c, t) \<Rightarrow> t'; 0 \<turnstile> c; s = t (< l)\<rbrakk>
+       \<Longrightarrow> s' = t' (< l)"
+  sorry
+(*proof(induction arbitrary: t t' rule: big_step_induct)
+case (Skip s)
+  then show ?case by auto
+next
+  case (Assign x a s)
+  have [simp]:"t' = t(x := aval a t)" using Assign sorry
+  have "sec a \<le> sec x"  using \<open>0 \<turnstile> x ::= a\<close> by auto
+  show ?case 
+  proof auto
+(* 1. sec x \<le> l \<Longrightarrow> aval a s = aval a t
+ 2. \<And>xa. xa \<noteq> x \<Longrightarrow> sec xa \<le> l \<Longrightarrow> s xa = t xa*)
+    assume " sec x < l"
+    have "sec a < l" 
+      using \<open>sec a \<le> sec x\<close> \<open>sec x < l\<close> by linarith
+    thus " aval a s = aval a t" 
+      using Assign.prems(3) aval_eq_if_eq_le dual_order.strict_trans2 by blast
+  next
+    fix xa assume "xa \<noteq> x" "sec xa < l"
+    thus "s xa = t xa" 
+      by (simp add: Assign.prems(3))
+  qed
+next
+  case (Seq c\<^sub>1 s\<^sub>1 s\<^sub>2 c\<^sub>2 s\<^sub>3)
+  then show ?case by blast
+next
+  case (IfTrue b s c\<^sub>1 t c\<^sub>2)
+  have "sec b \<turnstile> c\<^sub>1" "sec b \<turnstile> c\<^sub>2" using \<open>0 \<turnstile> IF b THEN c\<^sub>1 ELSE c\<^sub>2\<close> by auto
+  show ?case 
+    proof cases
+      assume "sec b \<le> l"
+      then have "s = t (\<le> sec b)" 
+        using IfTrue.prems(3) \<open>sec b \<le> l\<close> by auto
+      hence "bval b t" 
+        using IfTrue.hyps(1) bval_eq_if_eq_le by blast
+      with IfTrue.IH IfTrue.prems(1,3) \<open>sec b \<turnstile> c\<^sub>1\<close> anti_mono
+      show ?thesis by (auto)
+    next
+      assume "\<not> sec b \<le> l"
+      have "sec b \<turnstile> IF b THEN c\<^sub>1 ELSE c\<^sub>2" 
+        by (simp add: If \<open>sec b \<turnstile> c\<^sub>1\<close> \<open>sec b \<turnstile> c\<^sub>2\<close>)
+      then have "t = t' (\<le> l)" 
+        using IfTrue.prems(1) \<open>\<not> sec b \<le> l\<close> confinement by auto
+      then show ?thesis 
+        using IfTrue.hyps(2) IfTrue.prems(3) \<open>\<not> sec b \<le> l\<close> \<open>sec b \<turnstile> c\<^sub>1\<close> confinement by fastforce
+    qed
+next
+  case (IfFalse b s c\<^sub>2 t c\<^sub>1)
+  have "sec b \<turnstile> c\<^sub>1" "sec b \<turnstile> c\<^sub>2" using \<open>0 \<turnstile> IF b THEN c\<^sub>1 ELSE c\<^sub>2\<close> by auto
+  show ?case 
+    proof cases
+      assume "sec b \<le> l"
+      then have "s = t (\<le> sec b)" 
+        using IfFalse.prems(3) le_trans by blast
+      hence "\<not>bval b t" 
+        using IfFalse.hyps(1) bval_eq_if_eq_le by auto
+      with IfFalse.IH IfFalse.prems(1,3) \<open>sec b \<turnstile> c\<^sub>2\<close> anti_mono
+      show ?thesis by blast
+    next
+      assume "\<not> sec b \<le> l"
+      have "sec b \<turnstile> IF b THEN c\<^sub>1 ELSE c\<^sub>2" 
+        by (simp add: If \<open>sec b \<turnstile> c\<^sub>1\<close> \<open>sec b \<turnstile> c\<^sub>2\<close>)
+      then have "t = t' (\<le> l)" 
+        using IfFalse.prems(1) \<open>\<not> sec b \<le> l\<close> confinement by auto
+      then show ?thesis 
+        using IfFalse.hyps(2) IfFalse.prems(3) \<open>\<not> sec b \<le> l\<close> \<open>sec b \<turnstile> c\<^sub>2\<close> confinement by fastforce
+    qed
+next
+  case (WhileFalse b s c)
+  have "sec b \<turnstile> c" 
+    using WhileFalse.prems(2) com.distinct(17) com.distinct(19) com.distinct(7) by force
+  show ?case 
+  proof cases
+    assume "sec b \<le> l"
+      then have "s = t (\<le> sec b)" 
+        using WhileFalse.prems(3) dual_order.trans by blast
+      hence "\<not>bval b t" 
+        using WhileFalse.hyps bval_eq_if_eq_le by blast
+    then show ?thesis 
+      using WhileFalse.prems(1) WhileFalse.prems(3) by blast
+  next
+    assume "\<not> sec b \<le> l"
+      have "sec b \<turnstile> WHILE b DO c" 
+        by (simp add: While \<open>sec b \<turnstile> c\<close>)
+      then have "t = t' (\<le> l)" 
+        using WhileFalse.prems(1) \<open>\<not> sec b \<le> l\<close> confinement by auto
+    then show ?thesis 
+      by (simp add: WhileFalse.prems(3))
+  qed
+next
+  case (WhileTrue b s\<^sub>1 c s\<^sub>2 s\<^sub>3 t1 t3)
+  have "sec b \<turnstile> c" 
+    using WhileTrue.prems(2) by force
+  show ?case 
+    proof cases
+      assume "sec b \<le> l"
+      then have "s\<^sub>1 = t1 (\<le> sec b)" 
+        using WhileTrue.prems(3) \<open>sec b \<le> l\<close> by auto
+      hence "bval b t1" 
+        using WhileTrue.hyps(1) bval_eq_if_eq_le by blast
+      then obtain t2 where "(c, t1) \<Rightarrow> t2" "(WHILE b DO c , t2 ) \<Rightarrow> t3" 
+        using WhileTrue.prems(1) by auto
+      with WhileTrue.IH(1,2) WhileTrue.prems(2, 3) anti_mono
+      show ?thesis by auto
+    next
+      assume "\<not> sec b \<le> l"
+      have "sec b \<turnstile> WHILE b DO c" 
+        by (simp add: While \<open>sec b \<turnstile> c\<close>)
+      then have "t1 = t3 (\<le> l)" 
+        using WhileTrue.prems(1) \<open>\<not> sec b \<le> l\<close> confinement max_absorb1 by auto
+      with WhileTrue.hyps(2,3) WhileTrue.prems(3) \<open>\<not> sec b \<le> l\<close> \<open>sec b \<turnstile> WHILE b DO c\<close> \<open>sec b \<turnstile> c\<close> 
+      show ?thesis 
+        by (smt confinement dual_order.strict_trans not_le_imp_less order.not_eq_order_implies_strict)
+    qed
+  qed
+*)
 end
