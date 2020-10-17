76132ef732d8a12a5df5771fc5a32a9b74f014e6
2222-42 <stephen0617@me.com>
2020-10-17 16:56:41 +0900

    feat(chap10): prove D_preservation

--- ConcreteSemanticsChapter10/ConcreteSemantics10_1_Def_Init_Small.thy
+++ ConcreteSemanticsChapter10/ConcreteSemantics10_1_Def_Init_Small.thy
@@ -75,7 +75,42 @@ case (While b A c A')
 qed*)
 
 lemma D_mono:  "D A c M \<Longrightarrow> A \<subseteq> A' \<Longrightarrow> \<exists>M'. D A' c M' & M <= M'"
-  sorry
+proof(induction c arbitrary: A A' M)
+case SKIP
+  then show ?case by (fastforce intro: D.Skip)
+next
+  case (Assign x1 x2)
+  then show ?case by (fastforce intro: D.Assign)
+next
+  case (Seq c1 c2)
+  then show ?case by auto (metis D.intros(3))
+(*by (fastforce intro: D.Seq)*)
+next
+  case (If x1 c1 c2)
+(*
+ 1. \<And>x1 c1 c2 A A' M.
+       (\<And>A A' M. D A c1 M \<Longrightarrow> A \<subseteq> A' \<Longrightarrow> \<exists>M'. D A' c1 M' \<and> M \<subseteq> M') \<Longrightarrow>
+       (\<And>A A' M. D A c2 M \<Longrightarrow> A \<subseteq> A' \<Longrightarrow> \<exists>M'. D A' c2 M' \<and> M \<subseteq> M') \<Longrightarrow>
+       D A (IF x1 THEN c1 ELSE c2) M \<Longrightarrow> A \<subseteq> A' \<Longrightarrow> \<exists>M'. D A' (IF x1 THEN c1 ELSE c2) M' \<and> M \<subseteq> M'
+*)
+  then obtain M1 M2 where "vars x1 \<subseteq> A" "D A c1 M1" "D A c2 M2" "M = M1 \<inter> M2" 
+    by blast
+(* use IH Mx to  Mx' and A to A'*)
+  then obtain M1' M2' where "D A' c1 M1'" "D A' c2 M2'"  "M1 \<subseteq> M1'" "M2 \<subseteq> M2'" 
+    by (meson If.IH(1) If.IH(2) If.prems(2))
+(*  then have "D A (IF x1 THEN c1 ELSE c2) M" 
+    using If.prems(1) by blast*)
+  then have "D A' (IF x1 THEN c1 ELSE c2) (M1' \<inter> M2')" "M \<subseteq> M1' \<inter> M2'" sledgehammer
+    using \<open>vars x1 \<subseteq> A\<close> \<open>A \<subseteq> A'\<close> \<open>M = M1 \<inter> M2\<close> by(fastforce intro: D.intros)+
+(*    using D.If If.prems(2) \<open>D A' c1 M1'\<close> \<open>D A' c2 M2'\<close> \<open>vars x1 \<subseteq> A\<close> apply blast*)
+  then show ?case 
+    by meson (*by (fastforce intro: D.If)*)
+next
+  case (While x1 c)
+  then show ?case 
+    by auto (metis D.intros(5) subset_trans)
+    (*by (fastforce intro: D.While)*)
+qed
 
 theorem D_preservation:
   "(c,s) \<rightarrow> (c',s') \<Longrightarrow> D (dom s) c A \<Longrightarrow> \<exists>A'. D (dom s') c' A' & A <= A'"
@@ -97,7 +132,22 @@ next
   then show ?case by (auto intro: D.intros)
 next
   case (While b c s)
-  then show ?case sorry
+(*
+proof (state)
+this:
+  D (dom s) (WHILE b DO c) A
+
+goal (1 subgoal):
+ 1. \<And>b c s A. D (dom s) (WHILE b DO c) A \<Longrightarrow> \<exists>A'. D (dom s) (IF b THEN c;; WHILE b DO c ELSE SKIP) A' \<and> A \<subseteq> A'
+*)
+  then obtain A' where A': "vars b \<subseteq> dom s" "A = dom s" "D (dom s) c A'" by blast
+  then obtain A'' where "D A' c A''" by (metis D_incr D_mono)
+  with A' have "D (dom s) (IF b THEN c;; WHILE b DO c ELSE SKIP) (dom s)" 
+    by (metis D.If[OF \<open>vars b \<subseteq> dom s\<close> D.Seq[OF \<open>D (dom s) c A'\<close> D.While[OF _ \<open>D A' c A''\<close>]] D.Skip] D_incr Int_absorb1 subset_trans)
+    (* not shown only from sledgehammer *)
+  then show ?case 
+    by (metis A'(2) D_increase)
+(*    by (smt D.simps D_increase While.prems com.distinct(13) com.distinct(17) com.distinct(19)) (*> 1.0s*)*)
 qed
 
 (* Theorem 10.4 (D is sound). *)
