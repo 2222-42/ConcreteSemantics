ddaa461b49b555b0bb6e2420917818ef29e6125d
2222-42 <stephen0617@me.com>
2020-11-07 14:53:22 +0900

    feat(chap10): proof Lemma 10.5 (Soundness of D).

--- ConcreteSemanticsChapter10/ConcreteSemantics10_1_Def_Init_Big.thy
+++ ConcreteSemanticsChapter10/ConcreteSemantics10_1_Def_Init_Big.thy
@@ -27,4 +27,56 @@ WhileTrue:
 
 lemmas big_step_induct = big_step.induct[split_format(complete)]
 
+subsection "Soundness wrt Big Steps"
+
+text\<open>Note the special form of the induction because one of the arguments
+of the inductive predicate is not a variable but the term \<^term>\<open>Some s\<close>:\<close>
+
+(* Lemma 10.5 (Soundness of D). *)
+theorem Sound:
+  "\<lbrakk> (c,Some s) \<Rightarrow> s';  D A c A';  A \<subseteq> dom s \<rbrakk>
+  \<Longrightarrow> \<exists> t. s' = Some t \<and> A' \<subseteq> dom t"
+proof (induction c "Some s" s' arbitrary: s A A' rule:big_step_induct)
+(* I failed to show the case of None *)
+(*  case None
+  then show ?case sorry
+next*)
+  case Skip
+  then show ?case by fastforce
+next
+  case (AssignNone a s x)
+  then show ?case 
+    by auto (metis aval_Some option.simps(3) subset_trans)
+next
+  case (Assign a s i x)
+  then show ?case by auto
+next
+  case (Seq c\<^sub>1 s\<^sub>2 c\<^sub>2 s\<^sub>3)
+  then show ?case by auto metis
+next
+  case (IfNone b s c\<^sub>1 c\<^sub>2)
+  then show ?case 
+    by auto (metis bval_Some option.simps(3) order_trans)
+next
+  case (IfTrue b s c\<^sub>1 s' c\<^sub>2)
+  then show ?case by auto blast
+next
+  case (IfFalse b s c\<^sub>2 s' c\<^sub>1)
+  then show ?case by auto blast
+next
+  case (WhileNone b s c)
+  then show ?case 
+    by auto (metis bval_Some option.simps(3) order_trans)
+next
+  case (WhileFalse b s c)
+  then show ?case by auto
+next
+  case (WhileTrue b s c s' s'')
+  from \<open>D A (WHILE b DO c) A'\<close> obtain A' where "D A c A'" by blast
+  then obtain t' where "s' = Some t'" "A \<subseteq> dom s" 
+    using WhileTrue.hyps(3) WhileTrue.prems(2) by blast
+  then show ?case 
+    by (meson D_incr WhileTrue.hyps(3) WhileTrue.hyps(5) WhileTrue.prems(1) \<open>D A c A'\<close> dual_order.trans)
+qed
+
 end
