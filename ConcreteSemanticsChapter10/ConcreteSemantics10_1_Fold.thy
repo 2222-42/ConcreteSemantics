5bd04eb1212c7930e81a081af11ead1f69a60f12
2222-42 <stephen0617@me.com>
2020-11-11 16:15:38 +0900

    feat(chap10): proove Lemma 10.6 (Correctness of afold).

--- ConcreteSemanticsChapter10/ConcreteSemantics10_1_Fold.thy
+++ ConcreteSemanticsChapter10/ConcreteSemantics10_1_Fold.thy
@@ -12,4 +12,23 @@ fun afold :: "aexp \<Rightarrow> tab \<Rightarrow> aexp" where
 "afold (Plus e1 e2) t = (case (afold e1 t, afold e2 t) of
   (N n1, N n2) \<Rightarrow> N(n1+n2) | (e1',e2') \<Rightarrow> Plus e1' e2')"
 
+definition  "approx t s \<longleftrightarrow> (\<forall> x k. t x = Some k \<longrightarrow> s x = k)"
+
+(*Lemma 10.6 (Correctness of afold).*)
+theorem aval_afold[simp]:
+assumes "approx t s"
+shows "aval (afold a t) s = aval a s"
+proof (induction a)
+  case (N x)
+  then show ?case by auto 
+next
+  case (V x)
+  then show ?case using assms by (auto simp: approx_def split: option.split )
+next
+  case (Plus a1 a2)
+  then show ?case using assms by (auto simp: approx_def split: option.split aexp.split)
+qed
+  
+  
+
 end
