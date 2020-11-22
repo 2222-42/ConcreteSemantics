4d3838d43ced532bb8d07e057816fe50982f9e9b
2222-42 <stephen0617@me.com>
2020-11-22 13:55:48 +0900

    feat(chap10): proove Lemma 10.15 and 10.16

--- ConcreteSemanticsChapter10/ConcreteSemantics10_3_Live.thy
+++ ConcreteSemanticsChapter10/ConcreteSemantics10_3_Live.thy
@@ -11,4 +11,47 @@ fun L :: "com \<Rightarrow> vname set \<Rightarrow> vname set" where
 "L (IF b THEN c\<^sub>1 ELSE c\<^sub>2) X = vars b \<union> L c\<^sub>1 X \<union> L c\<^sub>2 X" |
 "L (WHILE b DO c) X = vars b \<union> X \<union> L c X"
 
+value "show (L (''y'' ::= V ''z'';; ''x'' ::= Plus (V ''y'') (V ''z'')) {''x''})"
+
+value "show (L (WHILE Less (V ''x'') (V ''x'') DO ''y'' ::= V ''z'') {''x''})"
+
+fun "kill" :: "com \<Rightarrow> vname set" where
+"kill SKIP = {}" |
+"kill (x ::= a) = {x}" |
+"kill (c\<^sub>1;; c\<^sub>2) = kill c\<^sub>1 \<union> kill c\<^sub>2" |
+"kill (IF b THEN c\<^sub>1 ELSE c\<^sub>2) = kill c\<^sub>1 \<inter> kill c\<^sub>2" |
+"kill (WHILE b DO c) = {}"
+
+fun gen :: "com \<Rightarrow> vname set" where
+"gen SKIP = {}" |
+"gen (x ::= a) = vars a" |
+"gen (c\<^sub>1;; c\<^sub>2) = gen c\<^sub>1 \<union> (gen c\<^sub>2 - kill c\<^sub>1)" |
+"gen (IF b THEN c\<^sub>1 ELSE c\<^sub>2) = vars b \<union> gen c\<^sub>1 \<union> gen c\<^sub>2" |
+"gen (WHILE b DO c) = vars b \<union> gen c"
+
+(* Lemma 10.15 (Liveness via gen/kill). *)
+lemma L_gen_kill: "L c X = gen c \<union> (X - kill c)"
+proof(induction c arbitrary:X)
+  case SKIP
+then show ?case by auto
+next
+  case (Assign x1 x2)
+  then show ?case by auto
+next
+  case (Seq c1 c2)
+  then show ?case by auto
+next
+  case (If x1 c1 c2)
+  then show ?case by auto
+next
+  case (While x1 c)
+  then show ?case by auto
+qed
+
+(*Lemma 10.15 (Liveness via gen/kill).*)
+lemma L_While_pfp: "L c (L (WHILE b DO c) X) \<subseteq> L (WHILE b DO c) X"
+  apply(auto simp add: L_gen_kill)
+  done
+
+
 end
