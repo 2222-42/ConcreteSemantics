d0fb1555fb427411b6971be3e523b7640325dacf
2222-42 <stephen0617@me.com>
2020-12-23 14:10:11 +0900

    chore(chap11): proof W_mono and D_While_If

--- ConcreteSemanticsChapter11/ConcreteSemantics11_Denotational.thy
+++ ConcreteSemanticsChapter11/ConcreteSemantics11_Denotational.thy
@@ -17,4 +17,26 @@ fun D :: "com \<Rightarrow> com_den" where
  = {(s,t). if bval b s then (s,t) \<in> D c1 else (s,t) \<in> D c2}" |
 "D (WHILE b DO c) = lfp (W (bval b) (D c))"
 
+lemma W_mono: "mono (W b r)"
+  apply(unfold mono_def W_def)
+  apply(auto)
+  done
+
+lemma D_While_If:
+  "D(WHILE b DO c) = D(IF b THEN c;;WHILE b DO c ELSE SKIP)"
+proof -
+  let ?w = "WHILE b DO c"
+  let ?f = "W (bval b) (D c)"
+  have "D ?w = lfp ?f" 
+    by simp
+  have "lfp ?f = ?f (lfp ?f)" 
+    by (simp add: W_mono def_lfp_unfold)
+  have "?f (lfp ?f) = ?f (D ?w)" 
+    by simp
+  have "?f (D ?w) = D(IF b THEN c;;WHILE b DO c ELSE SKIP)" 
+    using W_def by auto
+  then show ?thesis 
+    using \<open>lfp (W (bval b) (D c)) = W (bval b) (D c) (lfp (W (bval b) (D c)))\<close> by auto
+qed
+
 end
