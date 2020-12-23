27a28fe63a652dd02a090876ca34a66577f95879
2222-42 <stephen0617@me.com>
2020-12-23 13:50:03 +0900

    chore(chap11): add def of fun D

---
+++ ConcreteSemanticsChapter11/ConcreteSemantics11_Denotational.thy
@@ -0,0 +1,20 @@
+theory ConcreteSemantics11_Denotational
+  imports Main "~~/src/HOL/IMP/Big_Step"
+begin 
+
+section "Denotational Semantics of Commands"
+
+type_synonym com_den = "(state \<times> state) set"
+
+definition W :: "(state \<Rightarrow> bool) \<Rightarrow> com_den \<Rightarrow> (com_den \<Rightarrow> com_den)" where
+"W db dc = (\<lambda>dw. {(s,t). if db s then (s, t) \<in> dc O dw else s = t})"
+
+fun D :: "com \<Rightarrow> com_den" where
+"D SKIP   = Id" |
+"D (x ::= a) = {(s,t). t = s(x := aval a s)}" |
+"D (c1;;c2)  = D(c1) O D(c2)" |
+"D (IF b THEN c1 ELSE c2)
+ = {(s,t). if bval b s then (s,t) \<in> D c1 else (s,t) \<in> D c2}" |
+"D (WHILE b DO c) = lfp (W (bval b) (D c))"
+
+end
